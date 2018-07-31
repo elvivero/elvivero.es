c6007b9287fd1bcfcf45cf9e494d4fb1b34c219a
pichiruli <pichiruli@pichiruli-portatil.local>
2018-07-31 13:38:27 +0200

    responsive finished

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,33 +1,47 @@
-/* No drag! */
-$("body").css("display", "none");
-window.ondragstart = function() {
-  return false;
-}
-
 /* Contact */
 function displaycontact() {
-  $("#contact").css("visibility", "visible");
-  $("#contact").css("opacity", "1");
-  $("#back").css("visibility", "visible");
-  $("#map-canvas").css("display", "block");
-  $("#contact-link a").css("color", "white");
-  $("#contact-link a").css("background", "black");
+  $( "#contact" ).css( "visibility", "visible" );
+  $( "#contact" ).css( "opacity", "1" );
+  $( "#back" ).css( "visibility", "visible" );
+  $( "#map-canvas" ).css( "display", "block" );
+  $( "#contact-link a" ).css( "color", "white" );
+  $( "#contact-link a" ).css( "background", "black" );
 }
 function hidecontact() {
-  $("#contact").css("visibility", "hidden");
-  $("#contact").css("opacity", "0");
-  $("#back").css("visibility", "hidden");
-  $("#map-canvas").css("display", "none");
-  $("#contact-link a").css("color", "black");
-  $("#contact-link a").css("background", "transparent");
+  $( "#contact" ).css( "visibility", "hidden" );
+  $( "#contact" ).css( "opacity", "0" );
+  $( "#back" ).css( "visibility", "hidden" );
+  $( "#map-canvas" ).css( "display", "none" );
+  $( "#contact-link a" ).css( "color", "black" );
+  $( "#contact-link a" ).css( "background", "transparent" );
 }
 
 /* Select Menu */
 function SelectRedirect() {
-  var selects = document.getElementById("selectormenu");
-  var selectedValue = selects.options[selects.selectedIndex].value;
-  var element = document.getElementById("mySelect");
-  if (selectedValue != "") {
+  var selects = document.getElementById( "selectormenu" );
+  var selectedValue = selects.options[ selects.selectedIndex ].value;
+  var element = document.getElementById( "mySelect" );
+  if ( selectedValue != "" ) {
     window.location = selectedValue;
   }
 }
+
+
+
+  // No drag!
+  $( "body" ).css( "display", "none" );
+  window.ondragstart = function() {
+    return false;
+  };
+
+$( function() {
+  // Iframe responsive
+  $( "iframe" ).each( function() {
+    let el = $( this );
+    let p = el.attr( "width" ) / el.attr( "height" );
+    el.width( p * el.height() );
+    $( window ).resize( function() {
+      el.width( p * el.height() );
+    } );
+  } );
+} );
--- todo.md
+++ todo.md
@@ -2,6 +2,6 @@
 - Cambiar los titulos de los proyectos a inglés (Hasta museo marco, terminar...)
 - Google analytics (ya configurado, comprobar una vez colgada la web)
 - Rehacer el desplegable (si es posible automatizado)
-- Responsive iframes
-- Responsive página studio
 - Revisar links internos proyectos
+
+¬ Cuando se configure el dominio hay que quitar /elvivero/ de algún lado...va
